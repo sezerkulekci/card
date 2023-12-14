d9cdd4e18db1d56c626a642d44e7ff71da65749d
Sezer Kulekci <sezerklkci@gmail.com>
2023-12-14 17:44:29 +0300

    test

---
+++ cards/src/App.js
@@ -0,0 +1,56 @@
+import "bulma/css/bulma.css";
+import "./App.css";
+import Course from "./Course";
+import Angular from "./images/angular.png";
+import Bootstrap from "./images/bootstrap.png";
+import Csharp from "./images/csharp.jpeg";
+import Kompleweb from "./images/kompleweb.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <section className="hero is-link">
+        <div className="hero-body">
+          <p className="title">Kurslarm</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column">
+              <Course
+                image={Angular}
+                title="Angular"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore culpa eius, aliquam facere suscipit error praesentium sit incidunt blanditiis cupiditate, ratione omnis harum deserunt sequi dignissimos exercitationem corporis, beatae distinctio?"
+              />
+            </div>
+            <div className="column">
+              <Course
+                image={Bootstrap}
+                title="bootstrap"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore culpa eius, aliquam facere suscipit error praesentium sit incidunt blanditiis cupiditate, ratione omnis harum deserunt sequi dignissimos exercitationem corporis, beatae distinctio?"
+              />
+            </div>
+            <div className="column">
+              <Course
+                image={Csharp}
+                title="web"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore culpa eius, aliquam facere suscipit error praesentium sit incidunt blanditiis cupiditate, ratione omnis harum deserunt sequi dignissimos exercitationem corporis, beatae distinctio?"
+              />
+            </div>
+            <div className="column">
+              <Course
+                image={Kompleweb}
+                title="FrontEnd"
+                description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore culpa eius, aliquam facere suscipit error praesentium sit incidunt blanditiis cupiditate, ratione omnis harum deserunt sequi dignissimos exercitationem corporis, beatae distinctio?"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default App;
